101932a04c67fe5deb3e773cb7cd4911a5b9102e
miguelillo <miguel.navarrovera@gmail.com>
2015-03-11 13:31:49 +0100

    Modificacion Entidad Telefonos.

--- src/org/mig/java/Entities/Telefonos.java
+++ src/org/mig/java/Entities/Telefonos.java
@@ -20,23 +20,12 @@ import javax.persistence.Table;
 /**
  * @author miguelangel
  */
-@Entity
-@Table(name = "telefonos")
-@NamedQueries({
-    @NamedQuery(name = "Telefonos.findAll", query = "SELECT t FROM Telefonos t")})
 public class Telefonos implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Basic(optional = false)
-    @Column(name = "Numero")
+  
     private int numero;
-    @Id
-    @Basic(optional = false)
-    @Column(name = "UsuarioMail")
     private String usuarioMail;
-    @JoinColumn(name = "UsuarioMail", referencedColumnName = "Mail", insertable = false, updatable = false)
-    @OneToOne(optional = false)
     private Usuarios usuarios;
-
+    
     public Telefonos() {
     }
 
@@ -71,31 +60,6 @@ public class Telefonos implements Serializable {
 
     public void setUsuarios(Usuarios usuarios) {
         this.usuarios = usuarios;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (usuarioMail != null ? usuarioMail.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Telefonos)) {
-            return false;
-        }
-        Telefonos other = (Telefonos) object;
-        if ((this.usuarioMail == null && other.usuarioMail != null) || (this.usuarioMail != null && !this.usuarioMail.equals(other.usuarioMail))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "org.mig.java.Entities.Telefonos[ usuarioMail=" + usuarioMail + " ]";
     }
 
 }
