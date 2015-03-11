a5d535e22cd87bd9d46bf4a8610997f5c09605db
miguelillo <miguel.navarrovera@gmail.com>
2015-03-11 13:28:02 +0100

    Modificacion Entidad ModaMujer

--- src/org/mig/java/Entities/ModaMujer.java
+++ src/org/mig/java/Entities/ModaMujer.java
@@ -18,15 +18,9 @@ import javax.persistence.Table;
 /**
  * @author miguelangel
  */
-@Entity
-@Table(name = "moda_mujer")
-@NamedQueries({
-    @NamedQuery(name = "ModaMujer.findAll", query = "SELECT m FROM ModaMujer m")})
+
 public class ModaMujer implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @Column(name = "Categoria")
+
     private String categoria;
 
     public ModaMujer() {
@@ -44,29 +38,5 @@ public class ModaMujer implements Serializable {
         this.categoria = categoria;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (categoria != null ? categoria.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof ModaMujer)) {
-            return false;
-        }
-        ModaMujer other = (ModaMujer) object;
-        if ((this.categoria == null && other.categoria != null) || (this.categoria != null && !this.categoria.equals(other.categoria))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "org.mig.java.Entities.ModaMujer[ categoria=" + categoria + " ]";
-    }
 
 }
