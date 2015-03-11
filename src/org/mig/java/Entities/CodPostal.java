7478f153611f32cedd661a38d4176dc772fec29e
miguelillo <miguel.navarrovera@gmail.com>
2015-03-11 13:22:28 +0100

    Modificacion Entidad CodPostal
    He quitado los comentarios de Cod postal

--- src/org/mig/java/Entities/CodPostal.java
+++ src/org/mig/java/Entities/CodPostal.java
@@ -18,15 +18,9 @@ import javax.persistence.Table;
 /**
  * @author miguelangel
  */
-@Entity
-@Table(name = "cod_postal")
-@NamedQueries({
-    @NamedQuery(name = "CodPostal.findAll", query = "SELECT c FROM CodPostal c")})
+
 public class CodPostal implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @Column(name = "Cod_Postal")
+    
     private Integer codPostal;
 
     public CodPostal() {
@@ -43,30 +37,4 @@ public class CodPostal implements Serializable {
     public void setCodPostal(Integer codPostal) {
         this.codPostal = codPostal;
     }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (codPostal != null ? codPostal.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof CodPostal)) {
-            return false;
-        }
-        CodPostal other = (CodPostal) object;
-        if ((this.codPostal == null && other.codPostal != null) || (this.codPostal != null && !this.codPostal.equals(other.codPostal))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "org.mig.java.Entities.CodPostal[ codPostal=" + codPostal + " ]";
-    }
-
 }
